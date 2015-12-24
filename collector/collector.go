5565fdb2981e32971bc00022e4fd3fe4fc65eafc
Jan Halfar <jan@bestbytes.com>
2015-12-24 18:06:39 +0100

    prepared watcher and collector

--- collector/collector.go
+++ collector/collector.go
@@ -1,5 +1,153 @@
 package collector
 
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/foomo/petze/config"
+	"github.com/foomo/petze/watch"
+)
+
+// Collector collects stats on services
+type Collector struct {
+	servicesConfigfile string
+	peopleConfigfile   string
+	chanPeople         chan map[string]*config.Person
+	chanServices       chan map[string]*config.Service
+	watchers           map[string]*watch.Watcher
+	results            map[string][]*watch.Result
+}
+
 type Feedback struct {
 	Status int
+}
+
+func NewCollector(servicesConfigfile string, peopleConfigfile string) (c *Collector, err error) {
+	c = &Collector{
+		servicesConfigfile: servicesConfigfile,
+		peopleConfigfile:   peopleConfigfile,
+		chanPeople:         make(chan map[string]*config.Person),
+		chanServices:       make(chan map[string]*config.Service),
+		watchers:           make(map[string]*watch.Watcher),
+		results:            make(map[string][]*watch.Result),
+	}
+	go c.collect()
+	go c.configWatch()
+	return c, nil
+}
+
+const maxResults = 10
+
+func (c *Collector) checkResults(services map[string]*config.Service) {
+	for serviceID, service := range services {
+		results, ok := c.results[serviceID]
+		if !ok {
+			log.Println("wtf no results for", serviceID)
+			continue
+		}
+		for index, result := range results {
+			log.Println(service.ID, index, result.Error)
+		}
+	}
+}
+
+func (c *Collector) collect() {
+
+	people := make(map[string]*config.Person)
+	services := make(map[string]*config.Service)
+	chanResult := make(chan *watch.Result)
+
+	for {
+		select {
+		case newPeople := <-c.chanPeople:
+			people = newPeople
+			log.Println("updated people", newPeople, people)
+		case newServices := <-c.chanServices:
+			services = newServices
+			// stop old watchers
+			for oldWatcherID, oldWatcher := range c.watchers {
+				oldWatcher.Stop()
+				delete(c.watchers, oldWatcherID)
+			}
+			// setup new watches
+			for serviceID, service := range services {
+				c.watchers[serviceID] = watch.Watch(service, chanResult)
+				_, ok := c.results[serviceID]
+				if !ok {
+					c.results[serviceID] = []*watch.Result{}
+				}
+			}
+			// clean up results
+			for possiblyUnknownServiceId, _ := range c.results {
+				_, ok := c.watchers[possiblyUnknownServiceId]
+				if !ok {
+					// clean up results
+					delete(c.results, possiblyUnknownServiceId)
+				}
+			}
+			log.Println("updated services")
+		case result := <-chanResult:
+			log.Println("got a result", result)
+			results, ok := c.results[result.ID]
+			if ok {
+				results = append(results, result)
+				if len(results) > maxResults {
+					results = results[len(results)-maxResults:]
+				}
+				c.results[result.ID] = results
+			}
+			c.checkResults(services)
+		case <-time.After(time.Second * 10):
+			log.Println("----------------- heartbeat ----------------")
+			c.checkResults(services)
+		}
+	}
+}
+
+func getLastChange(filename string) int64 {
+	c := int64(0)
+	info, err := os.Stat(filename)
+	if err == nil {
+		c = info.ModTime().UnixNano()
+	}
+	return c
+}
+
+func (c *Collector) configWatch() {
+	serviceLastChange := int64(0)
+	peopleLastChange := int64(0)
+	for {
+		newServiceLastChange := getLastChange(c.servicesConfigfile)
+		newPeopleLastChange := getLastChange(c.peopleConfigfile)
+		if newPeopleLastChange > peopleLastChange {
+			c.updatePeople()
+			peopleLastChange = newPeopleLastChange
+		}
+		if newServiceLastChange > serviceLastChange {
+			c.updateServices()
+			serviceLastChange = newServiceLastChange
+		}
+		time.Sleep(time.Second)
+	}
+}
+
+func (c *Collector) updatePeople() error {
+	people, err := config.LoadPeople(c.peopleConfigfile)
+	if err == nil {
+		c.chanPeople <- people
+	} else {
+		log.Println("could not update people", err)
+	}
+	return err
+}
+
+func (c *Collector) updateServices() error {
+	services, err := config.LoadServices(c.servicesConfigfile)
+	if err == nil {
+		c.chanServices <- services
+	} else {
+		log.Println("could not update services", err)
+	}
+	return err
 }
--- watch/watch.go
+++ watch/watch.go
@@ -1,7 +1,67 @@
 package watch
 
+import (
+	"fmt"
+	"net/http"
+	"time"
 
+	"github.com/foomo/petze/config"
+)
 
-func Watch(service config.Service, feedbackChannel chan ) {
+type Result struct {
+	ID         string
+	Error      error
+	DNS        bool
+	Timestamp  time.Time
+	RunTime    time.Duration
+	StatusCode int
+}
 
+type Watcher struct {
+	active  bool
+	service *config.Service
+}
+
+// Watch create a watcher and start watching
+func Watch(service *config.Service, chanResult chan *Result) *Watcher {
+	w := &Watcher{
+		active:  true,
+		service: service,
+	}
+	go w.watchLoop(chanResult)
+	return w
+}
+
+// Stop watching - beware this is async
+func (w *Watcher) Stop() {
+	w.active = false
+}
+
+func (w *Watcher) watchLoop(chanResult chan *Result) {
+	for w.active {
+		r := watch(w.service)
+		if w.active {
+			chanResult <- r
+			time.Sleep(time.Second * time.Duration(w.service.Interval))
+		}
+	}
+}
+
+// actual watch
+func watch(service *config.Service) (r *Result) {
+	r = &Result{
+		ID:        service.ID,
+		Timestamp: time.Now(),
+	}
+	response, err := http.DefaultClient.Get(service.Endpoint)
+	r.RunTime = time.Since(r.Timestamp)
+	if err != nil {
+		r.Error = err
+		return
+	}
+	r.StatusCode = response.StatusCode
+	if response.StatusCode != http.StatusOK {
+		r.Error = fmt.Errorf("unexpected status code: %d", response.StatusCode)
+	}
+	return
 }
