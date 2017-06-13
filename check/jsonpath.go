320f552e65487a301a09176e0057fa0a2a00fa3e
Jan Halfar <jan@bestbytes.com>
2017-06-13 12:59:06 +0200

    json path minMaxCount and string equals

---
+++ check/jsonpath.go
@@ -0,0 +1,82 @@
+package check
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/Jeffail/gabs"
+	"github.com/NodePrime/jsonpath"
+	"github.com/foomo/petze/config"
+)
+
+func JSONPath(jsonBytes []byte, selector string, expect config.Expect) (ok bool, info string) {
+	info = "check not implemented"
+
+	paths, errParsePaths := jsonpath.ParsePaths(selector)
+	if errParsePaths != nil {
+		info = "could not parse json paths : " + errParsePaths.Error()
+		return
+	}
+
+	eval, errEval := jsonpath.EvalPathsInBytes(jsonBytes, paths)
+	if errEval != nil {
+		info = "error in json path : " + errEval.Error()
+		return
+	}
+
+	result, evalOK := eval.Next()
+	if !evalOK {
+		info = "no results for json path: " + selector
+		return
+	}
+
+	json, jsonErr := gabs.ParseJSON(result.Value)
+	if jsonErr != nil {
+		info = "could not parse json: " + jsonErr.Error()
+		return
+	}
+
+	//fmt.Println(json.Data()) // true -> show keys in pretty string
+	data := json.Data()
+	refl := reflect.ValueOf(data)
+	length := int64(-1)
+	resultIsString := false
+	resultString := ""
+	switch refl.Kind().String() {
+	case "string":
+		resultIsString = true
+		resultString = data.(string)
+	case "slice":
+		length = int64(len(data.([]interface{})))
+	default:
+		fmt.Println("wtf would that be", refl.Type().String(), refl.Kind().String())
+	}
+
+	if eval.Error != nil {
+		info = "could not evaluate json path " + eval.Error.Error()
+		return
+	}
+
+	switch true {
+	case expect.Min != nil, expect.Max != nil, expect.Count != nil:
+		return checkMinMaxCount(expect, int64(length))
+	case expect.Equals != nil:
+		expected := ""
+		if reflect.ValueOf(expect.Equals).Type().String() != "string" {
+			info = "jsonpath can only compare to string"
+			return
+		}
+		expected = expect.Equals.(string)
+		if !resultIsString {
+			info = "result is not a string"
+			return
+		}
+		if resultString == expected {
+			ok = true
+			return
+		}
+		info = "actual: " + resultString + " != expected: " + expected
+		return
+	}
+	return
+}
