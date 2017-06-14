e57138758a6a8a0d0ea81809c40e238ae29a5279
Stefan Martinov <stefan.martinov@gmail.com>
2017-06-14 14:43:22 +0200

    Remove optional arguments from petze CLI

--- petze.go
+++ petze.go
@@ -5,22 +5,36 @@ import (
 	"log"
 	"os"
 
-	"github.com/foomo/petze/config"
 	"github.com/foomo/petze/service"
+	"fmt"
+	"github.com/foomo/petze/config"
 )
 
 func main() {
-	flagConfigDir := flag.String("config-dir", "", "config-dir")
+	flag.Usage = usage
 	flag.Parse()
-
-	if *flagConfigDir == "" {
+	if flag.NArg() != 1 {
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	serverConfig, err := config.LoadServer(*flagConfigDir)
+	configurationDirectory := os.Args[1]
+	if stat, err := os.Stat(configurationDirectory); err == nil && stat.IsDir() {
+		runServer(configurationDirectory)
+	} else {
+		log.Fatal("specified configuration directory does not exist or is not a directory")
+	}
+}
+
+func runServer(configurationDirectory string) {
+	serverConfig, err := config.LoadServer(configurationDirectory)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println(service.Run(serverConfig, *flagConfigDir))
+	log.Println(service.Run(serverConfig, configurationDirectory))
+}
+
+func usage() {
+	fmt.Printf("Usage: %s configuration-directory \n", os.Args[0])
+	flag.PrintDefaults()
 }
