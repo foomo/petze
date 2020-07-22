7047d634bec6a155c07d1251190acf6d49541d9f
Philippe Partarrieu <ppartarr@outlook.com>
2020-07-22 18:36:53 +0200

    Add slack integration
    Using slack incoming webhooks, we can post messages to a slack channel
    by simply creating a slack app and enabling the incoming webhook:
    https://api.slack.com/messaging/webhooks

---
+++ slack/slack.go
@@ -0,0 +1,93 @@
+package slack
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/sirupsen/logrus"
+	prefixed "github.com/x-cray/logrus-prefixed-formatter"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
+)
+
+var (
+	Log     = logrus.New()
+	webhook string
+)
+
+type Message struct {
+	Text string `json:"text"`
+}
+
+func init() {
+	Log.Formatter = &prefixed.TextFormatter{
+		ForceColors:     true,
+		ForceFormatting: true,
+	}
+}
+
+// ConfigureLogger toggles debugging and adds full timestamps in production mode
+func ConfigureLogger(level logrus.Level, prod bool) {
+
+	if prod {
+		Log.Formatter = &prefixed.TextFormatter{
+			ForceColors:     true,
+			ForceFormatting: true,
+			FullTimestamp:   true,
+			TimestampFormat: "Mon 2 Jan 2006 15:04:05",
+		}
+	}
+
+	Log.Level = level
+}
+
+const timestampFormat = "Mon 2 Jan 2006 15:04:05"
+
+func InitSlackBot(w string) {
+	webhook = w
+}
+
+func Send(message []byte) {
+	client := &http.Client{}
+	requestBody := bytes.NewReader(message)
+	request, err := http.NewRequest("POST", webhook, requestBody)
+	request.Header.Add("Content-type", "application/json")
+
+	// execute the HTTP request
+	response, err := client.Do(request)
+	if err != nil {
+		Log.Error(err)
+	}
+	defer response.Body.Close()
+
+	// read response body
+	responseBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		Log.Error(err.Error())
+	}
+	Log.Info("slack bot response body: ", string(responseBody))
+}
+
+func GenerateErrorMessage(errs []error) []byte {
+
+	var errMessage = []string{
+		time.Now().Format(timestampFormat),
+		"an error occured:\n",
+	}
+
+	if len(errs) > 0 {
+		for _, e := range errs {
+			errMessage = append(errMessage, e.Error())
+		}
+	}
+
+	unmarshalledMessage := &Message{Text: strings.Join(errMessage, " ")}
+
+	marshalledMessage, err := json.Marshal(unmarshalledMessage)
+	if err != nil {
+		Log.Error(err)
+	}
+
+	return marshalledMessage
+}
